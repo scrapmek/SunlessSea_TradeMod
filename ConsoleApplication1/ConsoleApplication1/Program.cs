0581d69328e05b2ce249e9ea61ed62da5a82e8dc
ScrapMek <scrapmek@gmail.com>
2017-01-13 20:22:07 +0100

    Done!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,42 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-using System.Threading.Tasks;
 
 namespace TradeMod
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string file = getExchangesFile();
+            string file = getOriginalExchangesFile();
             string json;
             using (StreamReader reader = new StreamReader(file))
             {
-                 json = reader.ReadToEnd();
+                json = reader.ReadToEnd();
 
             }
 
             string result = duplicateSellingValues(json);
-            Console.WriteLine("");
+
+            Directory.CreateDirectory(getAddonExchangesFolder());
+            string path = getAddonExchangesFile();
+            
+            using (FileStream stream = File.Create(path))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.Write(result);
+            }
+
+            printProgress(100);
+            Console.WriteLine(String.Format("{0}Modding complete. Press any key to continue...", Environment.NewLine));
             Console.ReadLine();
+
         }
 
         private static string duplicateSellingValues(string s)
         {
-            string salePricePattern = "\"SellPrice:\"\\s\\d+";
+            double progress = 0;
+            double lastProgress = -1;
 
 
-            s.
+            for (double i = 50000; i > 0; i--)
+            {
+                string oldSubString = String.Format("\"SellPrice\":{0},", i);
+                string newSubString = String.Format("\"SellPrice\":{0},", i * 2);
+                s = s.Replace(oldSubString, newSubString);
+                progress = (((50000 - i) / 50000) * 100);
+                if (Math.Floor(progress) > lastProgress)
+                {
+                    printProgress(Math.Floor(progress));
+                    lastProgress = Math.Floor(progress);
+                }
+
+            }
+
+            return s;
         }
 
-        private static string getExchangesFile()
+        private static void printProgress(double percentage)
         {
-            return @"C: \Users\ScrapMek\AppData\LocalLow\Failbetter Games\Sunless Sea\entities\exchanges.json";
-            //return Path.GetFullPath("~/entities/exchanges.json");
+            Console.Clear();
+            string progressMessage = String.Format("Doubling SalePrice values: {0}% complete", Math.Floor(percentage));
+            Console.WriteLine(progressMessage);
+        }
+
+        private static string getAddonExchangesFile()
+        {
+            string addonDirectory = getAddonExchangesFolder();
+            string path = Path.Combine(addonDirectory, @"exchanges.json");
+            return path;
+        }
+
+        private static string getAddonExchangesFolder()
+        {
+            string sunlessSeaDirectory = getSunlessSeaDirectory();
+            string path = Path.Combine(sunlessSeaDirectory, @"addon\Trade Mod\entities");
+            return path;
+        }
+
+        private static string getOriginalExchangesFile()
+        {
+            string sunlessSeaDirectory = getSunlessSeaDirectory();
+            string path = Path.Combine(sunlessSeaDirectory, @"entities\exchanges.json");
+            return path;
+        }
+
+        private static string getSunlessSeaDirectory()
+        {
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            directory = Directory.GetParent(directory).FullName;
+            string path = Path.Combine(directory, @"LocalLow\Failbetter Games\Sunless Sea");
+            return path;
         }
     }
 }
